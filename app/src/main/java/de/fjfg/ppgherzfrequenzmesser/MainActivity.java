8fc5e641be359c50a9eece3b297cab4ed2da61fc
Florian Goelz <florian.goelz@outlook.de>
2021-04-22 15:22:02 +0200

    basic camera prompt

--- app/src/main/java/de/fjfg/ppgherzfrequenzmesser/MainActivity.java
+++ app/src/main/java/de/fjfg/ppgherzfrequenzmesser/MainActivity.java
@@ -1,14 +1,55 @@
 package de.fjfg.ppgherzfrequenzmesser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+
+    Button cameraPrompt = findViewById(R.id.cameraPrompt);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraPrompt.setOnClickListener(v -> onClick(cameraPrompt));
     }
+
+    private void onClick(View v){
+        if (hasCameraPermission()) {
+            enableCamera();
+        } else {
+            requestPermission();
+        }
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void enableCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
 }
