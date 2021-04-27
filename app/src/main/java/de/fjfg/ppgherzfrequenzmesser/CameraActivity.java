efc1cea0954196cd1a75b2cb1b06cee2a67c6b06
Fabsi23 <fabio.jungmann@gmx.de>
2021-04-27 17:34:18 +0200

    Analysis and popup

--- app/src/main/java/de/fjfg/ppgherzfrequenzmesser/CameraActivity.java
+++ app/src/main/java/de/fjfg/ppgherzfrequenzmesser/CameraActivity.java
@@ -1,6 +1,7 @@
 package de.fjfg.ppgherzfrequenzmesser;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.Instrumentation;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,11 +18,12 @@ import android.widget.TextView;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import androidx.annotation.NonNull;
@@ -37,6 +39,8 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import org.jtransforms.fft.DoubleFFT_1D;
+
 public class CameraActivity extends AppCompatActivity {
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
@@ -47,7 +51,7 @@ public class CameraActivity extends AppCompatActivity {
     List<Double> redvalues = new ArrayList<>();
     List<Bitmap> bitmaps = new ArrayList<>();
     final long OFFSET = 5000;
-    final long MEASURE_TIME = 10000;
+    final long MEASURE_TIME = 20000;
     final long WAIT_AFTER = 1000;
     //List<Integer> greenvalues = new ArrayList<>();
     //List<Integer> bluevalues = new ArrayList<>();
@@ -90,9 +94,9 @@ public class CameraActivity extends AppCompatActivity {
                 } else if (difference > OFFSET + WAIT_AFTER + 1000 && !printed) {
                     printed = true;
                     calculateValuesSmallBorder();
-                    calculateValuesLargeBorder();
-                    calculateValuesFullImage();
-                    calculateValuesOnePixel();
+                    //calculateValuesLargeBorder();
+                    //calculateValuesFullImage();
+                    //calculateValuesOnePixel();
                 }
                 image.close();
 //                @SuppressLint("UnsafeExperimentalUsageError") final Bitmap map = toBitmap(image.getImage());
@@ -146,6 +150,99 @@ public class CameraActivity extends AppCompatActivity {
         cam.getCameraControl().enableTorch(true);
     }
 
+    private void showPopup(String title, String text) {
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage(text);
+        dlgAlert.setTitle(title);
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+
+    private void testFourier(List<Double> averages) {
+        System.out.println("Listsize: " + averages.size());
+        System.out.println("Starting fourier...");
+        DoubleFFT_1D fft = new DoubleFFT_1D(averages.size());
+        double[] values = new double[averages.size() * 2];
+        for (int i = 0; i < averages.size(); i++) {
+            values[i] = averages.get(i);
+        }
+        fft.realForward(values);
+        String arrayStr = "";
+        for (int i = 0; i < values.length; i++) {
+            arrayStr += ", " + values[i];
+        }
+        arrayStr = arrayStr.substring(2);
+        System.out.println(arrayStr);
+    }
+
+    private double getHeartRateCountPeaks(List<Double> averages) {
+        List<Double> smoothValues = getSmoothValues(averages);
+        List<Integer> peaks = getPeaks(smoothValues);
+        return peaks.size() * (60 / (MEASURE_TIME / 1000));
+    }
+
+    private double getHeartRateDistance(List<Double> averages) {
+        double sampling_rate = averages.size() / (MEASURE_TIME / 1000);
+        double frequency;
+        List<Double> smoothValues = getSmoothValues(averages);
+        List<Integer> peaks = getPeaks(smoothValues);
+        List<Integer> distances = getDistances(peaks);
+        frequency = 1 / (getMedian(distances) / sampling_rate);
+        return frequency * 60;
+    }
+
+    private List<Double> getSmoothValues(List<Double> averages) {
+        List<Double> smooth = new ArrayList<>();
+        double[] currentValues = new double[7];
+        for (int i = 0; i < averages.size(); i++) {
+            currentValues[i % 7] = averages.get(i);
+            if (i >= 6) {
+                smooth.add(getMean(currentValues));
+            }
+        }
+        System.out.println("Smooth values: " + smooth);
+        return smooth;
+    }
+
+    private double getMean(double[] values) {
+        double avg = 0;
+        for (int i = 0; i < values.length; i++) {
+            avg += values[i];
+        }
+        return avg / values.length;
+    }
+
+    private List<Integer> getPeaks(List<Double> values) {
+        List<Integer> peakIndices = new ArrayList<>();
+        for (int i = 1; i < values.size() - 1; i++) {
+            if (values.get(i - 1) < values.get(i) && values.get(i) > values.get(i + 1)) {
+                peakIndices.add(i);
+            }
+        }
+        System.out.println("Peaks: " + peakIndices);
+        return peakIndices;
+    }
+
+    private List<Integer> getDistances(List<Integer> peakIndices) {
+        List<Integer> distances = new ArrayList<>();
+        for (int i = 0; i < peakIndices.size() - 1; i++) {
+            distances.add(peakIndices.get(i + 1) - peakIndices.get(i));
+        }
+        System.out.println("Distances: " + distances);
+        return distances;
+    }
+
+    private double getMedian(List<Integer> peakDistances) {
+        int size = peakDistances.size();
+        Collections.sort(peakDistances);
+        if (peakDistances.size() % 2 == 0) {
+            return (peakDistances.get(size / 2) + peakDistances.get(size / 2 - 1)) / 2;
+        }
+        System.out.println("Median: " + peakDistances.get(size / 2));
+        return peakDistances.get(size / 2);
+    }
+
     private void calculateValuesSmallBorder() {
         Log.i("RESULT", "Calculating Small Border...");
         List<Double> averages = new ArrayList<>();
@@ -179,6 +276,12 @@ public class CameraActivity extends AppCompatActivity {
         averageStr = averageStr.replace(",", ".");
         averageStr = averageStr.replace("%%", ", ");
         Log.i("RESULT", averageStr);
+
+        double distanceCalc = getHeartRateDistance(averages);
+        double countCalc = getHeartRateCountPeaks(averages);
+
+        String popuptext = "BPM via distance: " + distanceCalc + "\n" + "BPM via count: " + countCalc;
+        showPopup("Pulsfeedback", popuptext);
     }
 
     private void calculateValuesLargeBorder() {
@@ -252,8 +355,8 @@ public class CameraActivity extends AppCompatActivity {
         List<Integer> values = new ArrayList<>();
         int x, y;
         for (Bitmap map : bitmaps) {
-            x = map.getWidth()/2;
-            y = map.getHeight()/2;
+            x = map.getWidth() / 2;
+            y = map.getHeight() / 2;
             int color = map.getPixel(x, y);
             values.add((color & 0xff0000) >> 16);
         }
